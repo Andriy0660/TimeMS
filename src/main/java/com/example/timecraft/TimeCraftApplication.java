09f60bba3d7f03c5bf488dc5ebc1d8afaa5e107b
Andrii Snovyda <andrii@craftcodelab.com>
2024-07-04 11:15:56 +0300

    initial commit

---
+++ src/main/java/com/example/timecraft/TimeCraftApplication.java
@@ -0,0 +1,13 @@
+package com.example.timecraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeCraftApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TimeCraftApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/timecraft/TimeCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timecraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeCraftApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
