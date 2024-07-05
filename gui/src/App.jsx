377bb7072b5f948fb60973f1e502341fbd964d99
Andrii Snovyda <andrii@craftcodelab.com>
2024-07-05 15:44:09 +0300

    TMC-5 added mock pages and routing

--- gui/src/App.jsx
+++ gui/src/App.jsx
@@ -1,7 +1,34 @@
+import {createBrowserRouter, Navigate, RouterProvider} from "react-router-dom";
+import InfoPage from "./pages/InfoPage.jsx";
+import TimeLogPage from "./pages/TimeLogPage.jsx";
+import Root from "./Root.jsx";
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Navigate replace to="/app" />,
+    },
+    {
+      path: "/app",
+      element: <Root />,
+      children: [
+        {
+          path: "/app",
+          element: <Navigate replace to="/app/timelog" />,
+        },
+        {
+          path: "/app/timelog",
+          element: <TimeLogPage />,
+        },
+        {
+          path: "/app/info",
+          element: <InfoPage />,
+        },
+      ],
+    },
+  ]);
   return (
-    <div>time-craft</div>
-  )
+    <RouterProvider router={router} />
+  );
 }
-
-export default App
+export default App;
