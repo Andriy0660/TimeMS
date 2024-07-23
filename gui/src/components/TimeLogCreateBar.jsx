22670b964815694affe9bf4a11244bbe19d24f20
Andrii Snovyda <andrii@craftcodelab.com>
2024-07-23 14:47:43 +0300

    TMC-7 changed CreateBar: added Create button, removed startTime

--- gui/src/components/TimeLogCreateBar.jsx
+++ gui/src/components/TimeLogCreateBar.jsx
@@ -1,14 +1,13 @@
-import {Button, CircularProgress, IconButton, TextField} from "@mui/material";
-import {TimeField} from "@mui/x-date-pickers";
+import {Button, CircularProgress, TextField} from "@mui/material";
 import dayjs from "dayjs";
 import {useState} from "react";
-import RestoreIcon from '@mui/icons-material/Restore';
 import dateTimeService from "../utils/dateTimeService.js";
 
 export default function TimeLogCreateBar({onCreate}) {
-  const [startTime, setStartTime] = useState(dayjs());
   const [ticketAndDescription, setTicketAndDescription] = useState("");
   const [isCreating, setIsCreating] = useState(false);
+  const [isStarting, setIsStarting] = useState(false);
+
   const handleCreate = async () => {
     setIsCreating(true);
     const jiraIssuePattern = /^[A-Z]{2,}-\d+/;
@@ -20,33 +19,33 @@ export default function TimeLogCreateBar({onCreate}) {
       description = description.slice(ticket.length).trim();
     }
     try {
-      await onCreate({ticket, startTime: dateTimeService.getFormattedDateTime(startTime), description});
+      await onCreate({ticket, description});
     } finally {
       setIsCreating(false);
     }
     setTicketAndDescription("");
   }
+
+  const handleStart = async () => {
+    setIsStarting(true);
+    const jiraIssuePattern = /^[A-Z]{2,}-\d+/;
+    const match = ticketAndDescription.match(jiraIssuePattern);
+    let ticket = "";
+    let description = ticketAndDescription;
+    if (match) {
+      ticket = match[0];
+      description = description.slice(ticket.length).trim();
+    }
+    try {
+      await onCreate({ticket, startTime: dateTimeService.getFormattedDateTime(dayjs()), description});
+    } finally {
+      setIsStarting(false);
+    }
+    setTicketAndDescription("");
+  }
   return (
     <div className="flex items-center">
-      <div className="ml-2 my-2">
-        <TimeField
-          className="w-20"
-          label="Start"
-          size="small"
-          value={startTime}
-          onChange={(date) => {
-            if (date === null) {
-              setStartTime(null);
-            } else if (dayjs(date).isValid()) {
-              setStartTime(dayjs(date))
-            }
-          }}
-          format="HH:mm"
-        />
-      </div>
-      <IconButton size="small" onClick={() => setStartTime(dayjs())}><RestoreIcon fontSize="small" /></IconButton>
-
-      <div className="min-w-40 w-full mx-2 my-2">
+      <div className="w-full mx-2 my-2">
         <TextField
           className="w-full"
           label="Description"
@@ -55,10 +54,10 @@ export default function TimeLogCreateBar({onCreate}) {
           size="small"
           autoComplete="off"
           onKeyDown={async (e) => {
-            if (e.key === "Enter") {
-              if (startTime) {
-                await handleCreate();
-              }
+            if (e.ctrlKey && e.key === "Enter") {
+              await handleStart();
+            } else if (e.key === "Enter") {
+              await handleCreate();
             }
           }}
         />
@@ -68,7 +67,15 @@ export default function TimeLogCreateBar({onCreate}) {
           variant="outlined"
           onClick={handleCreate}
         >
-          {isCreating ? <CircularProgress size={25} /> : "Start"}
+          {isCreating ? <CircularProgress size={25} /> : "Create"}
+        </Button>
+      </div>
+      <div className="mx-2 my-2">
+        <Button
+          variant="outlined"
+          onClick={handleStart}
+        >
+          {isStarting ? <CircularProgress size={25} /> : "Start"}
         </Button>
       </div>
     </div>
