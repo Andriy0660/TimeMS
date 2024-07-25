012d17e5148b37b33f180a2b5e762dd4183b7692
Andrii Snovyda <andrii@craftcodelab.com>
2024-07-25 11:15:51 +0300

    TMC-9 added more clear separating between timelogs for different days

--- gui/src/components/TimeLogList.jsx
+++ gui/src/components/TimeLogList.jsx
@@ -12,7 +12,7 @@ export default function TimeLogList({
   const renderedTimeLogs = Object.keys(timeLogs).map(date => {
     const logsForDate = timeLogs[date];
     return (
-      <div key={date}>
+      <div key={date} className="mb-2 w-3/5 shadow-md bg-gray-50">
         {mode !== "Day" && <div className="ml-1 font-semibold text-gray-500 text-xs font-mono">{date}</div>}
         {logsForDate.map((timeLog) => {
           return (
@@ -33,12 +33,10 @@ export default function TimeLogList({
 
   return (
     <div className="m-4 flex flex-col items-center">
-      <div className="w-3/5 overflow-x-auto shadow-md bg-gray-50">
-        {Object.keys(timeLogs).length > 0 ? renderedTimeLogs :
-          <div className="p-1 text-center italic">
-            No logs...
-          </div>}
-      </div>
+      {Object.keys(timeLogs).length > 0 ? renderedTimeLogs :
+        <div className="w-3/5 shadow-md bg-gray-50 p-1 text-center italic">
+          No logs...
+        </div>}
     </div>
   );
 }
