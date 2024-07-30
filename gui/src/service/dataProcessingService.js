65204e1e1c242c3613a5192e792c7481db9debc5
Andrii Snovyda <andrii@craftcodelab.com>
2024-07-30 18:44:10 +0300

    TMC-10 made dataProcessingService for flexible timelogs grouping

---
+++ gui/src/service/dataProcessingService.js
@@ -0,0 +1,35 @@
+const dataProcessingService = {
+  group(data, groupOrder) {
+    let res = data;
+    groupOrder.forEach(groupBy => {
+      res = this.groupBy(res, groupBy);
+    })
+    res.groupOrder = groupOrder;
+    return res;
+  },
+  groupBy(data, key) {
+    return this.groupNested(data, key);
+  },
+  groupNested(data, key) {
+    if (!Array.isArray(data)) {
+      Object.keys(data).forEach(groupKey => {
+        data[groupKey] = this.groupNested(data[groupKey], key);
+      });
+      return data;
+    } else {
+      return this.groupList(data, key);
+    }
+  },
+  groupList(data, key) {
+    return data.reduce((result, item) => {
+      const groupKey = item[key];
+      if (!result[groupKey]) {
+        result[groupKey] = []
+      }
+      result[groupKey].push(item);
+      return result;
+    }, {});
+  },
+};
+
+export default dataProcessingService;
