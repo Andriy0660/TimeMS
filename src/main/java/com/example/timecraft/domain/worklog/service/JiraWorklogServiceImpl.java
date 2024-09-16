35fbddcd6c5eb132cfafe5e4f67478783835f9d8
Andrii Snovyda <andrii@craftcodelab.com>
2024-09-16 22:32:33 +0300

    TMC-30 refined JiraWorklogServiceImpl.java

--- src/main/java/com/example/timecraft/domain/worklog/service/JiraWorklogServiceImpl.java
+++ src/main/java/com/example/timecraft/domain/worklog/service/JiraWorklogServiceImpl.java
@@ -27,56 +27,14 @@ import lombok.Setter;
 
 @Service
 @RequiredArgsConstructor
-public class JiraWorklogServiceImpl implements JiraWorklogService{
+public class JiraWorklogServiceImpl implements JiraWorklogService {
   public static final DateTimeFormatter JIRA_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
   private final RestTemplate restTemplate;
   private final AppProperties appProperties;
   private final ObjectMapper objectMapper;
   private final SyncProgressService syncProgressService;
 
-  private static String getTextFromAdf(JsonNode node) {
-    StringBuilder text = new StringBuilder();
-
-    if (node.has("content")) {
-      for (JsonNode content : node.get("content")) {
-        String type = content.get("type").asText();
-
-        switch (type) {
-          case "hardBreak":
-            text.append("\n");
-            break;
-          case "paragraph":
-            if (!text.isEmpty()) {
-              text.append("\n");
-            }
-            break;
-          case "listItem":
-            text.append("\n- ");
-            break;
-          case "text":
-            String textContent = content.get("text").asText();
-            if (content.has("marks")) {
-              for (JsonNode mark : content.get("marks")) {
-                if (mark.get("type").asText().equals("code")) {
-                  textContent = "`" + textContent + "`";
-                  break;
-                }
-              }
-            }
-            text.append(textContent);
-            break;
-          default:
-            break;
-        }
-
-        if (content.has("content")) {
-          text.append(getTextFromAdf(content));
-        }
-      }
-    }
-    return text.toString().trim();
-  }
-
+  @Override
   public List<WorklogJiraDto> fetchAllWorkLogDtos() {
     List<WorklogJiraDto> allWorklogJiraDtos = new ArrayList<>();
     List<String> allKeys = fetchAllIssueKeys();
@@ -121,11 +79,7 @@ public class JiraWorklogServiceImpl implements JiraWorklogService{
   private JiraResponse fetchIssueKeysPage(int startAt, int maxResults) {
     String url = appProperties.getJira().getUrl() + "/rest/api/3/search?&startAt=" + startAt + "&maxResults=" + maxResults;
 
-    HttpHeaders headers = new HttpHeaders();
-    String auth = appProperties.getJira().getEmail() + ":" + appProperties.getJira().getToken();
-    String encodedAuth = Base64.getEncoder().encodeToString(auth.getBytes(StandardCharsets.UTF_8));
-    headers.set("Authorization", "Basic " + encodedAuth);
-    headers.set("Content-Type", "application/json");
+    HttpHeaders headers = getHttpHeaders();
 
     HttpEntity<String> entity = new HttpEntity<>(headers);
     ResponseEntity<JiraResponse> response = restTemplate.exchange(url, HttpMethod.GET, entity, JiraResponse.class);
@@ -133,13 +87,20 @@ public class JiraWorklogServiceImpl implements JiraWorklogService{
     return response.getBody();
   }
 
-  public List<WorklogJiraDto> fetchWorklogDtosForIssue(String issueKey) {
-    String url = appProperties.getJira().getUrl() + "/rest/api/3/issue/" + issueKey + "/worklog";
-
+  private HttpHeaders getHttpHeaders() {
     HttpHeaders headers = new HttpHeaders();
     String auth = appProperties.getJira().getEmail() + ":" + appProperties.getJira().getToken();
     String encodedAuth = Base64.getEncoder().encodeToString(auth.getBytes(StandardCharsets.UTF_8));
     headers.set("Authorization", "Basic " + encodedAuth);
+    headers.set("Content-Type", "application/json");
+    return headers;
+  }
+
+  @Override
+  public List<WorklogJiraDto> fetchWorklogDtosForIssue(String issueKey) {
+    String url = appProperties.getJira().getUrl() + "/rest/api/3/issue/" + issueKey + "/worklog";
+
+    HttpHeaders headers = getHttpHeaders();
 
     HttpEntity<String> entity = new HttpEntity<>(headers);
 
@@ -176,6 +137,49 @@ public class JiraWorklogServiceImpl implements JiraWorklogService{
     }
 
     return worklogJiraDtos;
+  }
+
+  private static String getTextFromAdf(JsonNode node) {
+    StringBuilder text = new StringBuilder();
+
+    if (node.has("content")) {
+      for (JsonNode content : node.get("content")) {
+        String type = content.get("type").asText();
+
+        switch (type) {
+          case "hardBreak":
+            text.append("\n");
+            break;
+          case "paragraph":
+            if (!text.isEmpty()) {
+              text.append("\n");
+            }
+            break;
+          case "listItem":
+            text.append("\n- ");
+            break;
+          case "text":
+            String textContent = content.get("text").asText();
+            if (content.has("marks")) {
+              for (JsonNode mark : content.get("marks")) {
+                if (mark.get("type").asText().equals("code")) {
+                  textContent = "`" + textContent + "`";
+                  break;
+                }
+              }
+            }
+            text.append(textContent);
+            break;
+          default:
+            break;
+        }
+
+        if (content.has("content")) {
+          text.append(getTextFromAdf(content));
+        }
+      }
+    }
+    return text.toString().trim();
   }
 
   @Getter
