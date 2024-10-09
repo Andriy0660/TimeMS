b5912a67f4a74b2fe7fe363783aef13db596a70e
Andrii Snovyda <andrii@craftcodelab.com>
2024-10-09 10:54:09 +0300

    TMC-49 moved ExclamationMark into ConfirmationModal

--- gui/src/components/ConfirmationModal.jsx
+++ gui/src/components/ConfirmationModal.jsx
@@ -1,6 +1,5 @@
 import {CircularProgress, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from "@mui/material";
 import Button from "@mui/material/Button";
-import {ExclamationMark} from "./icons/ExclamationMark.jsx";
 import {useState} from "react";
 
 export default function ConfirmationModal({
@@ -75,4 +74,22 @@ export default function ConfirmationModal({
       </DialogActions>
     </Dialog>
   );
+}
+
+function ExclamationMark() {
+  return <svg
+    className="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200"
+    aria-hidden="true"
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 20 20"
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+    />
+  </svg>;
 }
