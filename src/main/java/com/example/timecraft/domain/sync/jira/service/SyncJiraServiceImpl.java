21c6317e9b1e18c50c63fb245bf6b5d9b520df61
Andrii Snovyda <andrii@craftcodelab.com>
2024-10-17 10:51:36 +0300

    TMC-51 fixed some mistakes discovered during writing tests

--- src/main/java/com/example/timecraft/domain/sync/jira/service/SyncJiraServiceImpl.java
+++ src/main/java/com/example/timecraft/domain/sync/jira/service/SyncJiraServiceImpl.java
@@ -4,16 +4,15 @@ import java.time.Clock;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 
 import com.example.timecraft.core.exception.BadRequestException;
+import com.example.timecraft.core.exception.NotFoundException;
 import com.example.timecraft.domain.jira.worklog.dto.JiraWorklogUpdateDto;
 import com.example.timecraft.domain.jira.worklog.dto.JiraWorklogDto;
 import com.example.timecraft.domain.jira.worklog.service.JiraWorklogService;
@@ -30,6 +29,8 @@ import com.example.timecraft.domain.worklog.mapper.WorklogMapper;
 import com.example.timecraft.domain.worklog.persistence.WorklogEntity;
 import com.example.timecraft.domain.worklog.api.WorklogSyncService;
 import lombok.RequiredArgsConstructor;
+
+import static com.example.timecraft.domain.sync.jira.util.SyncJiraUtils.defaultWorklogStartTime;
 
 @Service
 @RequiredArgsConstructor
@@ -60,7 +61,7 @@ public class SyncJiraServiceImpl implements SyncJiraService {
   @Override
   public void syncIntoJira(final SyncIntoJiraRequest request) {
     final LocalDate date = request.getDate();
-    final LocalDateTime dateTime = LocalDateTime.of(date, LocalTime.of(10, 0));
+    final LocalDateTime dateTime = LocalDateTime.of(date, defaultWorklogStartTime);
     final String description = request.getDescription();
     final String ticket = request.getTicket();
 
@@ -73,7 +74,7 @@ public class SyncJiraServiceImpl implements SyncJiraService {
       throw new BadRequestException("There is no worklog associated with the given ticket");
     }
 
-    deleteWorklogsForTicket(worklogEntityList.stream().skip(1).toList(), ticket);
+    deleteWorklogs(worklogEntityList.stream().skip(1).toList());
     final JiraWorklogDto updated = jiraWorklogService.update(ticket, worklogEntityList.getFirst().getId(), JiraWorklogUpdateDto.builder()
         .started(JiraWorklogUtils.getJiraStartedTime(dateTime))
         .timeSpentSeconds(totalSpentSeconds)
@@ -90,13 +91,14 @@ public class SyncJiraServiceImpl implements SyncJiraService {
     }).reduce(0, Integer::sum);
   }
 
-  private void deleteWorklogsForTicket(final List<WorklogEntity> worklogEntityList, final String ticket) {
+  private void deleteWorklogs(final List<WorklogEntity> worklogEntityList) {
     try {
-      worklogEntityList.forEach(worklog -> jiraWorklogService.delete(ticket, worklog.getId()));
-    } catch (HttpClientErrorException.NotFound e) {
+      worklogEntityList.forEach(worklog -> {
+        jiraWorklogService.delete(worklog.getTicket(), worklog.getId());
+        worklogSyncService.delete(worklog);
+      });
+    } catch (NotFoundException e) {
       throw new BadRequestException("Synchronization mismatch");
-    } finally {
-      syncWorklogsForTicket(ticket);
     }
   }
 
