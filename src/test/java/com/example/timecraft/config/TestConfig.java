7686780e6374273c0da52d7142196df742467374
Andrii Snovyda <andrii@craftcodelab.com>
2024-11-06 11:35:24 +0200

    TMC-24 fixed bug with failing tests after adding @TestPropertySource to made dynamic port generation for WireMockServer as new context was created and tried to start new one wiremock server on same port

--- src/test/java/com/example/timecraft/config/TestConfig.java
+++ src/test/java/com/example/timecraft/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.testcontainers.containers.PostgreSQLContainer;
 
+import com.example.timecraft.core.config.AppProperties;
 import com.github.tomakehurst.wiremock.WireMockServer;
 
 @TestConfiguration
@@ -29,9 +30,11 @@ public class TestConfig {
   }
 
   @Bean
-  public WireMockServer wireMockServer() {
-    WireMockServer wireMockServer = new WireMockServer(9999);
+  public WireMockServer wireMockServer(final AppProperties properties) {
+    final WireMockServer wireMockServer = new WireMockServer(0);
     wireMockServer.start();
+    int port = wireMockServer.port();
+    properties.getJira().setUrl("http://localhost:" + port);
     return wireMockServer;
   }
 
