5fc110981fa57c333b636bf96373877bb71e7c4d
Andrii Snovyda <andrii@craftcodelab.com>
2024-11-12 09:21:02 +0200

    TMC-56 removed NavBar from LogInPage and SignUpPage

--- gui/src/Root.jsx
+++ gui/src/Root.jsx
@@ -1,10 +1,13 @@
-import {Outlet} from "react-router-dom";
+import {Outlet, useLocation} from "react-router-dom";
 import NavBar from "./components/NavBar.jsx";
 
 export default function Root() {
+  const location = useLocation();
+  const isAuthPage = location.pathname.match("login") || location.pathname.match("signup");
+
   return (
     <div>
-      <NavBar />
+      {!isAuthPage && <NavBar />}
       <Outlet />
     </div>
   );
