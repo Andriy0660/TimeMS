f6b0b67d11f94d8714a46e014ffc66c6a8642927
Andrii Snovyda <andrii@craftcodelab.com>
2024-11-22 12:32:57 +0200

    TMC-57 added fields validation on backend

--- src/main/java/com/example/timecraft/domain/config/service/ConfigServiceImpl.java
+++ src/main/java/com/example/timecraft/domain/config/service/ConfigServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.timecraft.domain.config.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.timecraft.core.exception.BadRequestException;
 import com.example.timecraft.domain.config.dto.ConfigGetResponse;
 import com.example.timecraft.domain.config.dto.ConfigUpdateExternalServiceRequest;
 import com.example.timecraft.domain.config.dto.ConfigUpdateJiraRequest;
@@ -27,11 +28,24 @@ public class ConfigServiceImpl implements ConfigService {
 
   @Override
   public void updateTimeConfig(final ConfigUpdateTimeRequest request) {
+    validateTimeConfig(request);
     final ConfigEntity config = configRepository.findAll().getFirst();
     config.setDayOffsetHour(request.getDayOffsetHour());
     config.setWorkingDayStartHour(request.getWorkingDayStartHour());
     config.setWorkingDayEndHour(request.getWorkingDayEndHour());
     configRepository.save(config);
+  }
+
+  private void validateTimeConfig(final ConfigUpdateTimeRequest request) {
+    if (request.getDayOffsetHour() < 0 || request.getDayOffsetHour() > 12) {
+      throw new BadRequestException("Invalid day offset hour");
+    }
+    if (request.getWorkingDayStartHour() < 0 || request.getWorkingDayStartHour() > 12) {
+      throw new BadRequestException("Invalid working day start hour");
+    }
+    if (request.getWorkingDayEndHour() < 12 || request.getWorkingDayEndHour() > 23) {
+      throw new BadRequestException("Invalid working day end hour");
+    }
   }
 
   @Override
@@ -43,10 +57,17 @@ public class ConfigServiceImpl implements ConfigService {
 
   @Override
   public void updateExternalServiceConfig(final ConfigUpdateExternalServiceRequest request) {
+    validateExternalServiceConfig(request);
     final ConfigEntity config = configRepository.findAll().getFirst();
     config.setIsExternalServiceEnabled(request.getIsExternalServiceEnabled());
     config.setExternalServiceTimeCf(request.getExternalServiceTimeCf());
     config.setIsExternalServiceIncludeDescription(request.getIsExternalServiceIncludeDescription());
     configRepository.save(config);
+  }
+
+  private void validateExternalServiceConfig(final ConfigUpdateExternalServiceRequest request) {
+    if (request.getExternalServiceTimeCf() <= 0) {
+      throw new BadRequestException("Invalid external service time coefficient");
+    }
   }
 }
