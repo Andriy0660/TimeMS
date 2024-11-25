0f7edd9c1ef7c762b91dfaf263c20cbdc26508fe
Andrii Snovyda <andrii@craftcodelab.com>
2024-11-25 10:34:06 +0200

    TMC-57 refactored validation for time fields

--- gui/src/components/config/ConfigTimeTab.jsx
+++ gui/src/components/config/ConfigTimeTab.jsx
@@ -2,7 +2,7 @@ import {Card, CardActions, CardContent, TextField, Typography} from "@mui/materi
 import Button from "@mui/material/Button";
 import {initializeConfig} from "../../config/config.js";
 import useAppContext from "../../context/useAppContext.js";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 export default function ConfigTimeTab({
   dayOffsetHour, setDayOffsetHour, workingDayStartHour, setWorkingDayStartHour,
@@ -10,72 +10,89 @@ export default function ConfigTimeTab({
 }) {
   const {forceRender} = useAppContext();
 
-  const [dayOffsetHourError, setDayOffsetHourError] = useState(false);
-  const [workingDayStartHourError, setWorkingDayStartHourError] = useState(false);
-  const [workingDayEndHourError, setWorkingDayEndHourError] = useState(false);
+  const [dayOffsetHourError, setDayOffsetHourError] = useState("");
+  const [workingDayStartHourError, setWorkingDayStartHourError] = useState("");
+  const [workingDayEndHourError, setWorkingDayEndHourError] = useState("");
+
+  const getIsInteger = (value) => /^\d+$/.test(value);
+
+  const validateDayOffset = (newDayOffsetHour) => {
+    const isInteger = getIsInteger(newDayOffsetHour);
+    if (!isInteger || newDayOffsetHour < 0 || newDayOffsetHour > 12 || newDayOffsetHour > workingDayEndHour) {
+      setDayOffsetHourError("Day offset hour must be in 0 - 12 range");
+    } else {
+      setDayOffsetHourError("");
+    }
+  };
+
+  const validateStartHourOfWorkingDay = (newWorkingDayStartHour) => {
+    const isInteger = getIsInteger(newWorkingDayStartHour);
+    if (!isInteger || newWorkingDayStartHour < 0 || newWorkingDayStartHour > 12) {
+      setWorkingDayStartHourError("Start hour of working day must be in 0 - 12 range");
+    } else {
+      setWorkingDayStartHourError("");
+    }
+  };
+
+  const validateEndHourOfWorkingDay = (newWorkingDayEndHour) => {
+    const isInteger = getIsInteger(newWorkingDayEndHour);
+    if (!isInteger || newWorkingDayEndHour < 12 || newWorkingDayEndHour > 23 || newWorkingDayEndHour < workingDayStartHour) {
+      setWorkingDayEndHourError("End hour of working day must be in 12 - 23 range");
+    } else {
+      setWorkingDayEndHourError("");
+    }
+  };
+
+  useEffect(() => {
+    validateDayOffset(dayOffsetHour)
+  }, [dayOffsetHour])
+
+  useEffect(() => {
+    validateStartHourOfWorkingDay(workingDayStartHour)
+  }, [workingDayStartHour])
+
+  useEffect(() => {
+    validateEndHourOfWorkingDay(workingDayEndHour)
+  }, [workingDayEndHour])
 
   return (
     <Card className="mb-4">
       <CardContent>
         <Typography variant="h6">Time Configuration</Typography>
         <TextField
-          error={dayOffsetHourError}
+          error={!!dayOffsetHourError}
+          helperText={dayOffsetHourError}
           label="Day Offset Hour (0-12)"
           type="number"
           value={dayOffsetHour}
-          onChange={(e) => {
-            const newDayOffsetHour = e.target.value;
-            const isInteger = /^\d+$/.test(newDayOffsetHour);
-            if (!isInteger || newDayOffsetHour < 0 || newDayOffsetHour > 12 || newDayOffsetHour > workingDayEndHour) {
-              setDayOffsetHourError(true);
-            } else {
-              setDayOffsetHourError(false);
-            }
-            setDayOffsetHour(newDayOffsetHour);
-          }}
+          onChange={(e) => setDayOffsetHour(e.target.value)}
           fullWidth
           className="my-2"
         />
         <TextField
-          error={workingDayStartHourError}
+          error={!!workingDayStartHourError}
+          helperText={workingDayStartHourError}
           label="Working Day Start Hour (0-12)"
           type="number"
           value={workingDayStartHour}
-          onChange={(e) => {
-            const newWorkingDayStartHour = e.target.value;
-            const isInteger = /^\d+$/.test(newWorkingDayStartHour);
-            if (!isInteger || newWorkingDayStartHour < 0 || newWorkingDayStartHour > 12) {
-              setWorkingDayStartHourError(true);
-            } else {
-              setWorkingDayStartHourError(false);
-            }
-            setWorkingDayStartHour(newWorkingDayStartHour);
-          }}
+          onChange={(e) => setWorkingDayStartHour(e.target.value)}
           fullWidth
           className="my-2"
         />
         <TextField
-          error={workingDayEndHourError}
+          error={!!workingDayEndHourError}
+          helperText={workingDayEndHourError}
           label="Working Day End Hour (12-23)"
           type="number"
           value={workingDayEndHour}
-          onChange={(e) => {
-            const newWorkingDayEndHour = e.target.value;
-            const isInteger = /^\d+$/.test(newWorkingDayEndHour);
-            if (!isInteger || newWorkingDayEndHour < 12 || newWorkingDayEndHour > 23 || newWorkingDayEndHour < workingDayStartHour) {
-              setWorkingDayEndHourError(true);
-            } else {
-              setWorkingDayEndHourError(false);
-            }
-            setWorkingDayEndHour(newWorkingDayEndHour);
-          }}
+          onChange={(e) => setWorkingDayEndHour(e.target.value)}
           fullWidth
           className="mt-2"
         />
       </CardContent>
       <CardActions className="ml-2">
         <Button
-          disabled={dayOffsetHourError || workingDayStartHourError || workingDayEndHourError}
+          disabled={!!dayOffsetHourError || !!workingDayStartHourError || !!workingDayEndHourError}
           variant="contained" color="primary"
           onClick={async () => {
             await updateTimeConfig({dayOffsetHour, workingDayStartHour, workingDayEndHour});
